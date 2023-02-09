b1caaade7fa45741036dfd9b43569346d5bec0c6
Andrei <ikar2008@hotmail.com>
2023-02-09 16:04:28 +0300

    Add project files.

---
+++ Class_State/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+State state = new();
+//state.Print();//ошибка так как метод private
+state.PrintInternal();//ошибки нет так как метод internal
+state.PrintPublic();
+
+class State//все равно, что internal State
+{
+    string defaultVar = "default";//все равно, что private string
+    private string privateVar = "private"; //поле доступно только из текущего класса
+    //доступно из текущего класса и производных классов, которые определены в этом проекте
+    protected private string protectedPrivateVar = "protected private";
+    protected string protectedVar = "protected";//доступно из текущего класса и производных классов
+    internal string internalVar = "internal";//доступно в любом месте текущего проекта
+    //доступно в любом месте текущего проекта и из классов-наследников в других проектах
+    protected internal string protectedInternalVar = "protected internal";
+    public string publicVar = "public";//доступно в любом месте программы, а так же для других программ и сборок
+
+    void Print() => WriteLine(defaultVar);
+    private void PrintPrivate() => WriteLine(privateVar);
+    protected private void PrintProtectedPrivate() => WriteLine(protectedPrivateVar);
+    protected void PrintProtected() => WriteLine(protectedVar);
+    internal void PrintInternal() => WriteLine(internalVar);
+    protected internal void PrintProtectedInternal() => WriteLine(protectedInternalVar);
+    public void PrintPublic() => WriteLine(publicVar);
+}
---
+++ HomeWork_28.01.2023/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+//Person person = new("Том");
+//инициализация для третьего способа
+//Person person = new() { Name = "Bred" };
+Person ted = new Person() { Name = "Ted", Age = 22 };
+WriteLine(person.Name);
+//WriteLine(person.Email);
+
+//первый способ
+/*public class Person
+{
+    public string Nmae { get; init; } = "Undefined";
+}*/
+
+//Второ1 способ
+/*public class Person
+{
+    public Person(string name) => Name = name;
+    public string Name { get; init; }
+}*/
+
+//третий способ
+/*public class Person
+{
+    public string Name { get; init; } = "";
+}*/
+
+//четвёртый способ
+
+/*public class Person
+{
+    string name = "";
+    public string Name
+    {
+        get{ return name; }
+        init
+        {
+            name = value;
+            Email = $"{value}@gmail.com";
+        }
+    }
+
+    public string Email { get; init; }
+}*/
+
+//required
+/*public class Person
+{ 
+  
+    public required string Name { get; set; }
+    public required int Age { get; set; }
+
+    
+}*/
